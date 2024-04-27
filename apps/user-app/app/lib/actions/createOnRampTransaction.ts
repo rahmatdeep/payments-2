2932bd19b924c9801a85f36e69cbc76263b3d2e2
rahmatdeep <rahmatdeep@gmail.com>
2024-04-27 16:15:34 +0530

    add onRamp server action

---
+++ apps/user-app/app/lib/actions/createOnRampTransaction.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth";
+import prisma from "@repo/db/client";
+
+export async function createOnRampTransaction(
+  amount: number,
+  provider: string
+) {
+  const session = await getServerSession(authOptions);
+  const token = Math.random().toString();
+  const userId = session.user.id;
+
+  if (!userId) {
+    return {
+      msg: "User Not logged in",
+    };
+  }
+
+  await prisma.onRampTransaction.create({
+    data: {
+      userId: Number(userId),
+      amount,
+      status: "Processing",
+      startTime: new Date(),
+      provider,
+      token,
+    },
+  });
+
+  return {
+    msg: "on ramp transaction added",
+  };
+}
